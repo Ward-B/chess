21bb65415756b54c042361cf7fb821dcf9ae040c
WardBannink <the.lizardmen@hotmail.com>
2025-03-31 16:14:37 +0200

    Initial commit

---
+++ Scripts/player.gd
@@ -0,0 +1,17 @@
+class_name Player
+extends Node2D
+
+@export var isWhite: bool = false
+@export var playerNum: int = -1
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _startTurn():
+	pass
+
+func _endTurn():
+	pass
---
+++ Scripts/turn_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+@export var _curPlayerNum: int = -1
+@export var _players: Array[Player] = []
+
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
